ada979c3878facd3f665462cc458ea915b89814a
MOOC <676924@users.noreply.github.com>
2020-03-09 17:46:32 +0100

    added comment to wait for loading data

--- final/app/ui.R
+++ final/app/ui.R
@@ -8,6 +8,7 @@ shinyUI(fluidPage(
         p("This demo shows how an n-gram model can be used to suggest the next word to type."),
         p("The algorithm uses n-grams and a back-off smoothing technique for unseen combinations of words."),
         h3("Try it out!"),
+        p("please allow a few seconds for the model to load properly"),
         textInput(inputId = "text", label = NULL, width = "40%", placeholder = "Try it out"),
         htmlOutput('predictions')
     )
